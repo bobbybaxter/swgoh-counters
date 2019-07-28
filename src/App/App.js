d86c62da3d4d1d4360a8ac3e2e517e84871825b7
Bob <bob.e.baxter@gmail.com>
2019-07-28 12:10:45 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <MyNavbar />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
