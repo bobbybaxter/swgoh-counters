c541b491a8f1e97d0488cf0614fa2b76511a8269
Bob <bob.e.baxter@gmail.com>
2019-07-28 21:16:09 -0500

    Updates

---
+++ precache-manifest.39feeca023bccdd66b40facb18e4e46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf72dd7c580cef850112e042eab7f4d1",
+    "url": "/swgoh-counters/index.html"
+  },
+  {
+    "revision": "7973fc9d42123fa17fd3",
+    "url": "/swgoh-counters/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c951035676b3a557ab47",
+    "url": "/swgoh-counters/static/css/main.f3157415.chunk.css"
+  },
+  {
+    "revision": "7973fc9d42123fa17fd3",
+    "url": "/swgoh-counters/static/js/2.712ad1c8.chunk.js"
+  },
+  {
+    "revision": "c951035676b3a557ab47",
+    "url": "/swgoh-counters/static/js/main.31284d6b.chunk.js"
+  },
+  {
+    "revision": "ddd0edbc27f0c8afbeeb",
+    "url": "/swgoh-counters/static/js/runtime~main.a8d86a92.js"
+  }
+]);
