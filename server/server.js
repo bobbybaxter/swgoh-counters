9a3e8663f199d9fe985f5bd0e24a19e8d5e9223c
Bob Baxter <bob.e.baxter@gmail.com>
2020-05-08 12:23:35 -0500

    fixed cors policy

--- server/server.js
+++ server/server.js
@@ -5,22 +5,33 @@ const server = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://saiastrange.com',
+  'https://localhost:3000',
   'http://saiastrange.com',
+  'https://saiastrange.com',
+  'http://www.saiastrange.com',
+  'https://www.saiastrange.com',
 ];
 
 module.exports = (app) => {
   const PORT = process.env.PORT || 5000;
 
   server.use(cors({
-    origin(origin, callback) {
+
+    origin: function originCheck(origin, callback) {
+      // allow requests with no origin like mobile apps or curl requests
+      if (!origin) {
+        return callback(null, true);
+      }
+
       if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not '
-                + 'allow access from the specified Origin.';
+        const msg = `The CORS policy for this site does not allow access from the specified Origin of ${origin}.`;
         return callback(new Error(msg), false);
       }
+      console.log('origin passed :>> ', origin);
       return callback(null, true);
     },
+
+    credentials: true,
   }));
 
   server.use(require('./routes')(app));
