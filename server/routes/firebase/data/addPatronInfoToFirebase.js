9b096ccc4923feee7c37250a02ef261542415d35
Bob Baxter <bob.e.baxter@gmail.com>
2021-06-24 11:34:50 -0500

    fix refresh token bug

--- server/routes/firebase/data/addPatronInfoToFirebase.js
+++ server/routes/firebase/data/addPatronInfoToFirebase.js
@@ -1,13 +1,13 @@
-module.exports = ({ firebaseDb }) => (user) => {
-  if (user.id) {
-    firebaseDb.ref(`users/${user.id}`).update({
+module.exports = ( { firebaseDb } ) => user => {
+  if ( user.id ) {
+    firebaseDb.ref( `users/${ user.id }` ).update( {
       accessToken: user.accessToken,
       expiresIn: user.expiresIn,
       patreonId: user.patreonId,
       patronStatus: user.patronStatus,
       refreshToken: user.refreshToken,
       tier: user.tier,
-    });
+    } );
   }
 
   return 'ok';
