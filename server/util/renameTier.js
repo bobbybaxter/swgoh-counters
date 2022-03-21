4c66240ba35847364c1b63625bebb39bdba10e80
Bob Baxter <bob.e.baxter@gmail.com>
2022-03-21 11:22:10 -0500

    added new Patreon Aurodium tier id

--- server/util/renameTier.js
+++ server/util/renameTier.js
@@ -3,9 +3,10 @@ module.exports = tierId => {
     4023873: 'Carbonite',
     4023966: 'Bronzium',
     4023977: 'Chromium',
-    4023982: 'Aurodium',
+    8436763: 'Aurodium', // new Aurodium tier
+    4023982: 'Aurodium', // TODO: remove tier when tier is deleted
     4023995: 'Kyber',
     4024096: 'Power Converter',
   };
-  return tiers[tierId];
+  return tiers[ tierId ];
 };
